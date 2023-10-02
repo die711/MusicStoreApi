a07ed2a3b73db727e9b4c0a221b5f9c312a4cc56
Diego Resendiz Ojeda <di_564@hotmail.com>
2023-10-02 00:44:30 -0700

    cambios concert

---
+++ MusicStore.DataAccess/Migrations/20231002074306_cambios concert.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicStore.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class cambiosconcert : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Concert",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreId = table.Column<long>(type: "bigint", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    DateEvent = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageUrl = table.Column<string>(type: "varchar(1000)", unicode: false, maxLength: 1000, nullable: true),
+                    Place = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    TicketsQuantity = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(11,2)", precision: 11, scale: 2, nullable: false),
+                    Finalized = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Concert", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Concert_Genre_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Concert_GenreId",
+                table: "Concert",
+                column: "GenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Concert");
+        }
+    }
+}
---
+++ MusicStore.Entities/Concert.cs
@@ -0,0 +1,15 @@
+﻿namespace MusicStore.Entities;
+
+public class Concert : EntityBase
+{
+    public long GenreId { get; set; }
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!; 
+    public DateTime DateEvent { get; set; }
+    public string? ImageUrl { get; set; }
+    public string Place { get; set; } = default!;
+    public int TicketsQuantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public bool Finalized { get; set; }
+    public Genre Genre { get; set; } = default!;
+}
