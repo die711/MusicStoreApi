3554769f90de1ceeda1f92b1419515fec0bd8b02
Diego Resendiz Ojeda <di_564@hotmail.com>
2023-10-07 00:45:14 -0700

    migraciones sales

---
+++ MusicStore.DataAccess/Migrations/20231007074320_sale.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicStore.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class sale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sale",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<long>(type: "bigint", nullable: false),
+                    ConcertId = table.Column<long>(type: "bigint", nullable: false),
+                    SaleDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OperationNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(11,2)", precision: 11, scale: 2, nullable: false),
+                    Quantity = table.Column<short>(type: "smallint", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sale", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sale_Concert_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concert",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sale_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sale_ConcertId",
+                table: "Sale",
+                column: "ConcertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sale_CustomerId",
+                table: "Sale",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sale");
+        }
+    }
+}
---
+++ MusicStore.Entities/Sale.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicStore.Entities;
+
+public class Sale : EntityBase
+{
+    public Customer Customer { get; set; } = default!;
+    public long CustomerId { get; set; }
+    public Concert Concert { get; set; } = default!;
+    public long ConcertId { get; set; }
+    public DateTime SaleDate { get; set; }
+    public string OperationNumber { get; set; } = default!;
+    public decimal Total { get; set; }
+    public short Quantity { get; set; }
+}
